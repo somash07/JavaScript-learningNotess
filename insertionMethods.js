6e7bd69f046738d1a5096f7e0e53d3102b9ccc94
Somash Manandhar <manandharsomash@gmail.com>
2023-12-04 19:25:34 +0545

    creating element and insertion

---
+++ insertionMethods.js
@@ -0,0 +1,12 @@
+//bacha tarika
+let a=document.getElementsByTagName('div')[0]
+a.innerHTML+="<h1>hello</h1>"
+
+//for loop haru ma use huna element create garera
+let div=document.createElement('div')
+div.innerHTML=("<h2>created element</h2>")
+// a.prepend(div)
+// a.apppend(div)
+// a.before(div)
+// a.after(div)
+a.replaceWith(div)
