13ded175001cba6e40066059a9618567b3c476c0
Somash Manandhar <manandharsomash@gmail.com>
2023-12-12 20:22:04 +0545

    NamasteJs:promiseChaining basics,example using promise

--- promiseTwo.js
+++ promiseTwo.js
@@ -1,9 +1,9 @@
-// const cart=["shoes","pants","books"]
+const cart=["shoes","pants","books"]
 
 // const promise=createOrder(cart);//createrOrder() returns and empty obj with some property data ie {data: undefined} and after whatever asyc time some data aaunxa.
-// promise.then((orderId)=>{
-//     proceedToPayment(orderId);
-// })// this callback is called whenever there is data inside of the promiseObject.ONLY ONCE
+promise.then((orderId)=>{
+    proceedToPayment(orderId);
+})// this callback is called whenever there is data inside of the promiseObject.ONLY ONCE
 
 
 const GITHUB_API="https://api.github.com/users/somash07"
@@ -11,4 +11,13 @@ const GITHUB_API="https://api.github.com/users/somash07"
 const user=fetch(GITHUB_API);//promise obj.
 
 user
+.then((data)=>{
+   return data.json();
+})
 .then((data)=>console.log(data))
+
+//promise chaining :return is must
+createOrder(cart)
+.then((orderId)=>proceedToPayment(orderId))
+.then((paymentInfo)=>showOrderSummary(paymentInfo))
+.then((paymentInfo)=>updateWallet(paymentInfo))
