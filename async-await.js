3a653a59e87b3cb418125eaf2a50a901f58e2c1a
Somash Manandhar <manandharsomash@gmail.com>
2023-12-14 20:43:12 +0545

    async-await basics

---
+++ async-await.js
@@ -0,0 +1,26 @@
+/*
+    Async fnx always returns a promise.
+*/
+async function learningAsync(){
+    let p1=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("data1")
+    },3000)
+})
+let p2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("data2")
+    },5000)
+})
+console.log("fetching p1")
+let p1res=await p1
+console.log("fetching p2")
+let p2res=await p2
+return[p1res,p2res]
+}
+
+console.log("welcome to asycfnx ")
+learningAsync().then((val)=>{
+    console.log(val)
+})
+
