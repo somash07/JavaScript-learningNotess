af0ff539bc58011dc882a115233ec6ea3cfb323d
Somash Manandhar <manandharsomash@gmail.com>
2024-01-03 20:17:39 +0545

    solving error

--- miniProjects/weather-app/app.js
+++ miniProjects/weather-app/app.js
@@ -1,44 +1,54 @@
+const apiKey='ee041b3a9b6011bfd50ae763be7a02bd'
+const card=document.querySelector('.card')
+const errHead=document.querySelector('.errHead')
+const searchBar=document.querySelector('form input')
 
-
-
-// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=kathmandu';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '484b3471c9msh96bf41474e8aa0fp175567jsn647f510aa9a6',
-// 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-// 	}
-// };
-// getdata =async()=>{
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
-// }
-// getdata()
 
 let btn=document.querySelector('.search-btn')
 btn.addEventListener('click',async (evt)=>{
 	evt.preventDefault();
-	let city=document.querySelector('form input')
-	console.log(city.value)
-
-	const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city.value}`;
-	const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '484b3471c9msh96bf41474e8aa0fp175567jsn647f510aa9a6',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	const city=searchBar.value
+	if(city===""){
+		displayError("BRO!!!enter a city.....")
 	}
-	};
-	
-	const response = await fetch(url, options);
-	let data=await response.json()
-	document.getElementById('temperature').innerHTML=data.temp
-			
-	
+	else{
+		try{
+			const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+			console.log(city.value)
+
+			const response=await fetch(url)
+			console.log(response)
+			const data=await response.json()
+			console.log(data)
+		}catch(err){
+			displayError("please enter a valid weather")
+		}
+	}
 })
+
+getWeatherData=async(city)=>{
+
+}
+
+displayWeather=(data)=>{
+
+}
+
+getWeatherImg=(weatherId)=>{
+
+}
+
+displayError=(msg)=>{
+	console.log("hiiii")
+	errHead.style.color="red"
+	errHead.innerHTML=msg
+	card.style.display="none"
+
+	setTimeout(()=>{
+		errHead.innerHTML=""
+	},2000)
+}
+
+
+
 
