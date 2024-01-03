caac8980e443e6caac9fc96b0df1932e41bd4e96
Somash Manandhar <manandharsomash@gmail.com>
2024-01-03 22:16:06 +0545

    last

--- miniProjects/weather-app/app.js
+++ miniProjects/weather-app/app.js
@@ -3,50 +3,50 @@ const card=document.querySelector('.card')
 const errHead=document.querySelector('.errHead')
 const searchBar=document.querySelector('form input')
 
+const tempBox=document.querySelector('temp-box')
+
 
 let btn=document.querySelector('.search-btn')
 btn.addEventListener('click',async (evt)=>{
 	evt.preventDefault();
 	const city=searchBar.value
 	if(city===""){
-		displayError("BRO!!!enter a city.....")
+		displayError("please enter a valid city.......")
 	}
 	else{
 		try{
-			const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+			const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 			console.log(city.value)
-
 			const response=await fetch(url)
 			console.log(response)
 			const data=await response.json()
 			console.log(data)
+
+			getWeatherData(data.main.temp)
+
 		}catch(err){
 			displayError("please enter a valid weather")
 		}
 	}
 })
 
-getWeatherData=async(city)=>{
-
-}
-
 displayWeather=(data)=>{
 
 }
 
-getWeatherImg=(weatherId)=>{
-
+getWeatherData=(temp)=>{
+	temp-box.innerHTML=temp
 }
 
 displayError=(msg)=>{
 	console.log("hiiii")
-	errHead.style.color="red"
+	errHead.style.color="#f08080"
 	errHead.innerHTML=msg
 	card.style.display="none"
 
 	setTimeout(()=>{
 		errHead.innerHTML=""
-	},2000)
+	},3000)
 }
 
 
