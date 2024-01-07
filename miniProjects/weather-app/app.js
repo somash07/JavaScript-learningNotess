54eecb28e76543ba44dba638ba888f84b3ffe11d
Somash Manandhar <manandharsomash@gmail.com>
2024-01-08 00:40:09 +0545

    handled err and adding time feature incomplete

--- miniProjects/weather-app/app.js
+++ miniProjects/weather-app/app.js
@@ -1,11 +1,13 @@
 
 
 const apiKey='ee041b3a9b6011bfd50ae763be7a02bd'
+const TimeapiKey='iJpNOuSTok39x1I/M2EFeQ==Zy8aSZkGfsLeWPuV'
 const card=document.querySelector('.card')
 const errHead=document.querySelector('.errHead')
 const searchBar=document.querySelector('form input')
 const tempBox=document.querySelector('.temp-box p')
 const weatherImage=document.querySelector('.weatherImg img')
+const datecontainer=document.querySelector('.datecontainer')
 
 
 let btn=document.querySelector('.search-btn')
@@ -18,12 +20,22 @@ btn.addEventListener('click',async (evt)=>{
 	else{
 		try{
 			const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+			const timeUrl=`https://api.api-ninjas.com/v1/worldtime?city=${city}`
 			console.log(city.value)
 			const response=await fetch(url)
 			// console.log(response)
+			let timeRes=await fetch(timeUrl,{
+				method:'GET',
+				headers: {
+					'X-Api-Key': TimeapiKey,
+					'Content-Type': 'application/json'
+				  }
+			})
+			const time=await timeRes.json()
+			console.log(time.date)
 			const data=await response.json()
 			console.log(data)
-			await getWeatherData(data,city)
+			await getWeatherData(data,city,time)
 			
 
 		}catch(err){
@@ -32,34 +44,29 @@ btn.addEventListener('click',async (evt)=>{
 	}
 })
 
-displayWeather=(data)=>{
 
-}
+function getWeatherData(data,city,time){
 
-function getWeatherData(data,city){
-
+	card.style.display="flex"
 	iconID=data.weather[0].id;
 
 	for(weatherInformation of weatherData){
 		if(iconID===Number.parseInt(weatherInformation.code)){
 			console.log("hi")
 			weatherImage.src = `https://openweathermap.org/img/wn/${weatherInformation.icon}@2x.png`;
-			weatherImage.style.height="15vh"
 			break;
 		}
 	}
-
+	datecontainer.innerHTML=(time.date)
 	temperature=(data.main.temp)
 	tempBox.innerHTML=`${temperature} &deg;C`
 	errHead.innerHTML=`Weather condition:  ${data.weather[0].description} in ${city}`
-	
-
 
 }
-displayError=(msg)=>{
+
+function displayError(msg){
 	console.log("hiiii")
 	errHead.innerHTML=msg
-	errHead.style.color="#f08080"
 	card.style.display="none"
 }
 
